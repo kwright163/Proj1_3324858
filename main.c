3105c2fa3dca9d09af8fcc50894288f5ef940e08
Kelly Wright <kelly.a.wright@uon.edu.au>
2019-04-04 05:45:35 +0000

    Starting pseudocode and plan for first task: encrypt message using rotation cipher given text message and key

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 main() {
+    
+   /*insert menu w 6 assessment options */
+   
+   
+   
+   /*TASK 1: ROTATION CIPHER: encrypt message provided cipher text and rotation amount */
+ 
+ printf("Enter a text message for encryption: \n");
+ scanf(%d, &/*store message in declared variable*/);
+ printf("Enter a key: \n");
+ scanf(%d, &/*store key in declared variable*/);  // message will be encrypted using alphabetical shift of input integer
+  
+  /* perform encryption */
+ 
+ printf("Encrypted message: \n", & /*store encrypted message in variable*/ );
 
   return 0;
 }
