dbdc5bb5b95f98ed4b533baa7bbc15b5f0cf0a98
Kelly Wright <kelly.a.wright@uon.edu.au>
2019-04-11 04:33:38 +0000

    initialising variables

--- main.c
+++ main.c
@@ -5,17 +5,22 @@ main() {
    /*insert menu w 6 assessment options */
    
    
-   
    /*TASK 1: ROTATION CIPHER: encrypt message provided cipher text and rotation amount */
+  
+ char message;
+ int key;
+ char encryption;
  
  printf("Enter a text message for encryption: \n");
- scanf(%d, &/*store message in declared variable*/);
+ scanf("%c", &message);
+ 
+ 
  printf("Enter a key: \n");
- scanf(%d, &/*store key in declared variable*/);  // message will be encrypted using alphabetical shift of input integer
+ scanf("%d", &key);  // message will be encrypted using alphabetical shift of input integer
   
   /* perform encryption */
  
- printf("Encrypted message: \n", & /*store encrypted message in variable*/ );
+ printf("Encrypted message: \n", &encryption);
 
   return 0;
 }
