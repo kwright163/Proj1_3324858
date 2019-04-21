b7c745cac5aff33320cada2af10d2e9631b17955
Kelly Wright <kelly.a.wright@uon.edu.au>
2019-04-21 07:54:44 +0000

    task 1

--- main.c
+++ main.c
@@ -16,7 +16,9 @@ int selection;
    
   scanf("%d", &selection); // compiler reads user's choice from above menu
 
- /* switch() */// switch() case indicates what compiler should execute for each of above menu options 
+ switch() //switch() case indicates what compiler should execute for each of above menu options 
+
+*/
 
 /*
   { 
@@ -26,60 +28,71 @@ int selection;
      
         printf("You selected option 1 \n");
       
-        char message[]; // inputted message array w unknown size (user inputs size), array structure: data_type array_name[array_size],
-        char character; // character: individual letter/character within string
-        int key, n;     // n: number that character is in string e.g. for 'hello', n=0 for 'h'
+        //INITIATING VARIABLES FOR ENCRYPTION
+        char message[100];  // inputted message array w max size of 100 characters, array structure: data_type array_name[array_size],
+        char character;     // character: individual letter/character within string
+        int key, n;         // n: number that character is in string e.g. for 'hello', n=0 for 'h'
             
-        /*
-        printf("Enter a text message for encryption in lower-case letters only: \n");
-        scanf("%c", message); //text message that will be encrypted using rotation cipher
-        printf("You entered: %d\n", message);
-            // TO DO: make %c read white/blank space
-            // TO DO: make uppercase letters work too 
+            
+        //RECEIVING ENCRYPTION MESSAGE FROM USER INPUT
+        printf("Enter a text message for encryption: \n");
+        scanf("%s", message);  //text message that will be encrypted using rotation cipher
+        printf("You entered: %s\n", message);
         
+        /* TO DO: make %s read white/blank space */
+        
+        
+        //RECEIVING KEY FROM USER
         printf("Enter a key: \n");
-        scanf("%d", &key);  // message will be encrypted using alphabetical shift of input integer
+        scanf("%d", key);   // message will be encrypted using alphabetical shift of input integer
+        
         printf("You entered: %d\n", &key);
-        */
-       
-       message[] = axzd; //TRIAL input message
-      
+                    
         /* IF: modulus used to take value of key/shift back to start of alphabet if key is equal to or larger than 26 */
         if (key >= 26)
         {
             key = key % 26; // e.g. if key = 29 = 29%26 = 3
+            printf("The key is %d\n", &key);
         }
+        
+        
+        //PERFORMING ENCRYPTION
         
         /*  INITIAL: start at first character of string
             CONDITION: make sure character does not have 'null' value
             INCREMENT: move to next letter in string   */
         for(n = 0; message[n] != '\0'; n++);
         {
-            if (65 <= character <= 90) //if user inputs capital letter (numeric boundaries are ASCII standard values)
+            if (character >= 65 && character <= 90) 
+            //if user inputs capital letter (numeric boundaries are ASCII standard values)
             {
-                character = character + key; //apply shift to character
+                character += key; //apply shift to character, operator shorthand(OS): character = character + key
                 
                 if (character > 90) // 90 is ASCII value for 'Z'
                 {
-                    character = character - 26; //wrap back to start of alphabet
+                    character -= 26; //wrap back to start of alphabet; 
                 }
                 
                 message[n] = character;
             }
 
                 
-            else if (97 <= character <= 122)
+            else if (character >= 97 && character <= 122)
             {
-                character = character + key; //apply shift to character
+                character += key; //apply shift to character
                 
                 if (character > 122) // 122 is ASCII value for 'z'
                 {
-                    character = character - 26; //wrap back to start of alphabet, as if 'z' is number prior to 'a'                }
+                    character -= 26; //wrap back to start of alphabet, as if 'z' is number prior to 'a'       
                 }
+                
+                message[n] = character;
             
             }
             
         }
+        
+        printf("Your encrypted message is: %s", message);
         
 /*
       case 3:
@@ -94,10 +107,8 @@ int selection;
 
 
       /* perform encryption */
-      /* print encryption: printf("Encrypted message: \n", &encryption); */
+      
 
-  }
-  
 
   return 0;
 }
