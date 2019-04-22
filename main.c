80f0c6d0ca5bae68f86631647aa787d9b0951b27
Kelly Wright <kelly.a.wright@uon.edu.au>
2019-04-22 00:02:29 +0000

    task 1

--- main.c
+++ main.c
@@ -28,87 +28,71 @@ int selection;
      
         printf("You selected option 1 \n");
       
-        //INITIATING VARIABLES FOR ENCRYPTION
         char message[100];  // inputted message array w max size of 100 characters, array structure: data_type array_name[array_size],
-        char character;     // character: individual letter/character within string
-        int key, n;         // n: number that character is in string e.g. for 'hello', n=0 for 'h'
-            
-            
-        //RECEIVING ENCRYPTION MESSAGE FROM USER INPUT
+   
+        //RECEIVING ENCRYPTION MESSAGE FROM USER
         printf("Enter a text message for encryption: \n");
-        scanf("%s", message);  //text message that will be encrypted using rotation cipher
-        printf("You entered: %s\n", message);
-        
-        /* TO DO: make %s read white/blank space */
-        
-        
-        //RECEIVING KEY FROM USER
+        scanf("%s", message); //compiler receives message from user input and stores in 'message
+
+    int key;
+    
+        //RECEVING KEY SHIT FROM user
         printf("Enter a key: \n");
-        scanf("%d", key);   // message will be encrypted using alphabetical shift of input integer
-        
-        printf("You entered: %d\n", &key);
-                    
-        /* IF: modulus used to take value of key/shift back to start of alphabet if key is equal to or larger than 26 */
+        scanf("%d", &key);
+    
+        //IF: modulus used to take value of key/shit back to start of alphabet if bigger than 25
         if (key >= 26)
         {
-            key = key % 26; // e.g. if key = 29 = 29%26 = 3
-            printf("The key is %d\n", &key);
+            key = key % 26;
+            printf("The key is %d\n", key);
+            //e.g. is key = 29, key = 29%26 = 3
         }
         
-        
-        //PERFORMING ENCRYPTION
-        
-        /*  INITIAL: start at first character of string
-            CONDITION: make sure character does not have 'null' value
-            INCREMENT: move to next letter in string   */
-        for(n = 0; message[n] != '\0'; n++);
+        else
         {
-            if (character >= 65 && character <= 90) 
-            //if user inputs capital letter (numeric boundaries are ASCII standard values)
-            {
-                character += key; //apply shift to character, operator shorthand(OS): character = character + key
-                
-                if (character > 90) // 90 is ASCII value for 'Z'
-                {
-                    character -= 26; //wrap back to start of alphabet; 
-                }
-                
-                message[n] = character;
-            }
-
-                
-            else if (character >= 97 && character <= 122)
-            {
-                character += key; //apply shift to character
-                
-                if (character > 122) // 122 is ASCII value for 'z'
-                {
-                    character -= 26; //wrap back to start of alphabet, as if 'z' is number prior to 'a'       
-                }
-                
-                message[n] = character;
-            
-            }
-            
+            key = key; //key value remans unchanged
+            printf("The key is %d\n", key);
         }
         
-        printf("Your encrypted message is: %s", message);
+    int n;          //n: number that character is in string e.g. for 'hello', n=0 for 'h'
+    char character; //character: individual character of message
+    
+    /*  INITIAL: start at first character of string
+        CONDITION: make sure character does not have 'null' value
+        INCREMENT: move to next letter in string   */
+        for (n = 0; message[n] != '\0'; n++)
+        {
+            character = message[n];
+            
+            if (65 <= character && character <= 90) //if user inputs uppercase letter (numeric boundaries are ASCII standard values)
+            {
+                character += key;       //apply shift to character, operator shorthand (OS): character = character + key
+                
+                if (character > 90)     //90 is ASCII value for 'Z'
+                {
+                    character -= 26;    //wrap back to start of alphabet; 
+                }
+                
+                message[n] = character; //assign shifted value of character to letter
+            }
+            
+            else if (character >= 97 && character <= 122) //if user inputs lowercase character
+            {
+                character += key;       //apply shift to character
+                
+                if (character > 122)    // 122 is ASCII value for 'z'
+                {
+                    character -= 26;    //wrap back to start of alphabet, as if 'z' is number prior to 'a'       
+                }
+                
+                message[n] = character; //assign shifted value of character to letter
+            }
+        }
+    
+    printf("Your encrypted message is: %s\n", message);
+    
+    printf("Your encrypted message is: %s\n", message);
         
-/*
-      case 3:
-      
-      case 4:
-      
-      case 5:
-      
-      case 6:
-      
-      default: //if user input doesn't match any of other options, perform following: 
-
-
-      /* perform encryption */
-      
-
 
   return 0;
 }
