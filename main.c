79cfca6824631f7fccc1ce147c5eb64fd59ce372
Kelly Wright <kelly.a.wright@uon.edu.au>
2019-04-23 08:10:44 +0000

    task 2 and switch()

--- main.c
+++ main.c
@@ -3,45 +3,36 @@
 main() {
     
 // MENU: prints menu from which reader must choose an encryption/decryption option
-/*
 int selection;
  
-  printf("Please select an option: \n");
-  printf("1) Encrypt message with rotation cipher given cipher text and rotation amount \n");
-  printf("2) Decrypt message encrypted with rotation cipher given cipher text and rotation amount \n");
-  printf("3) Encrypt message with substitution cipher given message text and alphabet substitution \n");
-  printf("4) Decrypt message encrypted with substitution cipher given cipher text and substitutions \n");
-  printf("5) Decrypt message encrypte with rotation cipher given cipher text only \n");
-  printf("6) Decrypt message encrypted with substitution cipher given cipher text only \n \n");
-   
-  scanf("%d", &selection); // compiler reads user's choice from above menu
+char message[100]; // inputted message array w max size of 100 characters, array structure: data_type array_name[array_size],
+char character;    // character: individual character of message
+int key, n;        // n: number that character is in string e.g. for 'hello', n=0 for 'h'
 
- switch() //switch() case indicates what compiler should execute for each of above menu options 
+    printf("Please select an option: \n");
+    printf("1) Encrypt message with rotation cipher given cipher text and rotation amount \n");
+    printf("2) Decrypt message encrypted with rotation cipher given cipher text and rotation amount \n");
+    printf("3) Encrypt message with substitution cipher given message text and alphabet substitution \n");
+    printf("4) Decrypt message encrypted with substitution cipher given cipher text and substitutions \n");
+    printf("5) Decrypt message encrypte with rotation cipher given cipher text only \n");
+    printf("6) Decrypt message encrypted with substitution cipher given cipher text only \n \n");
+    
+    scanf("%d", &selection); // compiler reads user's choice from above menu
 
-*/
-
-/*
-  { 
-      case 1: 
+switch(selection) //switch() case indicates what compiler should execute for each of above menu options 
+{
+    case 1: // Encryption of message w rotation cipher given message text and rotation amount
       
-      */ 
-     
         printf("You selected option 1 \n");
-      
-        char message[100]; // inputted message array w max size of 100 characters, array structure: data_type array_name[array_size],
-        char character;    // character: individual character of message
-        int key, n;        // n: number that character is in string e.g. for 'hello', n=0 for 'h'
-   
+     
         //RECEIVING ENCRYPTION MESSAGE FROM USER
         printf("Enter a text message for encryption: \n");
         scanf("%[^\n]s", message); // created 'input' file for user to input data for 'scanf()' to read and store in 'message' array
-        
-    
-        //RECEVING KEY SHIFT FROM USER
+           
+        //RECEIVING KEY SHIFT FROM USER
         printf("Enter a key: \n");
         scanf("%d", &key);
         
-    
         //IF: modulus used to take value of key/shit back to start of alphabet if bigger than 25
         if (key >= 26)
         {
@@ -52,15 +43,16 @@ int selection;
         {
             key = key; //key value remans unchanged
         }
+        //TO DO: work out if this if() section is necessary, alphabet wrapping around to beginnning may make this unnecessary
         
     
-    /*  INITIAL: start at first character of string
-        CONDITION: make sure character does not have 'null' value
-        INCREMENT: move to next letter in string   */
+        //  INITIAL: start at first character of string
+        //  CONDITION: make sure character does not have 'null' value
+        //  INCREMENT: move to next letter in string   
         for (n = 0; message[n] != '\0'; n++)
         {
             character = message[n];
-            
+        
             if (65 <= character && character <= 90) //if user inputs uppercase letter (numeric boundaries are ASCII standard values)
             {
                 character += key;       //apply shift to character, operator shorthand (OS): character = character + key
@@ -72,7 +64,7 @@ int selection;
                 
                 message[n] = character; //assign shifted value of character to letter
             }
-            
+                
             else if (character >= 97 && character <= 122) //if user inputs lowercase character
             {
                 character += key;       //apply shift to character
@@ -86,8 +78,73 @@ int selection;
             }
         }
     
-    printf("Your encrypted message is: %s\n", message);
+        printf("Your encrypted message is: %s\n", message);
+    
+        break;
         
-
+    
+    case 2: // Decryption of message encrypted w rotation cipher given cipher text and rotation amount
+    
+        printf("You selected option 2 \n");
+   
+        //RECEIVING DECRYPTION MESSAGE FROM USER
+        printf("Enter a text message for decryption: \n");
+        scanf("%[^\n]s", message); // created 'input' file for user to input data for 'scanf()' to read and store in 'message' array
+        
+        //RECEVING KEY SHIFT FROM USER
+        printf("Enter a key: \n");
+        scanf("%d", &key);
+        
+        //IF: modulus used to take value of key/shit back to start of alphabet if bigger than 25
+        if (key >= 26)
+        {
+            key = key % 26;  //e.g. if key = 29, key = 29%26 = 3
+        }
+        
+        else
+        {
+            key = key; //key value remans unchanged
+        }
+        //TO DO: work out if this if() section is necessary, alphabet wrapping around to beginnning may make this unnecessary
+        
+    
+        //  INITIAL: start at first character of string
+        //  CONDITION: make sure character does not have 'null' value
+        //  INCREMENT: move to next letter in string   
+        for (n = 0; message[n] != '\0'; n++)
+        {
+            character = message[n];
+            
+            if (character >= 65 && character <= 90) //if user inputs uppercase letter
+            {
+                character -= key;       //apply shift backwards to character
+                
+                if (character < 65)     //65 is ASCII value for 'A'
+                {
+                    character += 26;    //wrap back to end of alphabet, as if 'A' is number after 'Z'
+                }
+                
+                message[n] = character; //assign reverse-shifted value of character to letter
+            }
+            
+                
+            else if (character >= 97 && character <= 122) //if user inputs lowercase character
+            {
+                character -= key;       //apply shift backwards to character
+                
+                if (character < 97)    // 97 is ASCII value for 'a'
+                {
+                    character += 26;    //wrap back to start of alphabet, as if 'a' is number after to 'z'       
+                }
+                
+                message[n] = character; //assign reverse-shifted value of character to letter
+            }
+        }
+    
+        printf("Your decrypted message is: %s\n", message); 
+        
+        break;
+  
   return 0;
+}
 }
