49bd9133a4d0a649be41ca3d24308f66f1e4f9ed
Kelly Wright <kelly.a.wright@uon.edu.au>
2019-04-29 06:26:03 +0000

    block comment

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+/* This program is designed to encrypt and decrypt text messages using the rotation and substitution ciphers. 
+The user is required to read the menu printed on the screen and input the desired task number in the ‘input’ file in the compiler.
+
+The rotation cipher replaces each letter of the alphabet with another letter a  fixed number of places away in the alphabet. The key, or the ‘shift’, is the number 
+of letters that the alphabet is rotated by when producing the substitution letters. The user is required to input the message to be encrypted and the key shift into 
+the ‘input’ file. The compiler will then assign the number that the character is in the message (e.g. in the word “hello”, the letter ‘h’ would have the place ‘0’ 
+in the array, whilst ‘e’ would have the place ‘1’). The compiler will take the ASCII value of the input characters, add the key shift and then convert the number 
+back into ASCII values, before moving on to the next character in the input message, and so on. If the ASCII value overreaches the end of the alphabet, it will loop
+back to the start, hence ‘rotating’ the alphabet. The reverse is done when decrypting messages.
+
+The substitution cipher requires the user to input an encryption message and a ‘cipher’ alphabet – they must input all letters of the alphabet in any order. Each 
+letter in the straight ‘A’ to ‘Z’ alphabet is then replaced by the letter a corresponding numbers of places in the alphabet, e.g. if the cipher was 
+“QWERTYUIOPASDFGHJKLZXCVBNM”, then ‘A’ would be replaced by ‘Q’ and so on. The compiler will roll through alphabet until it finds a letter in the alphabet that is the
+same number of places through the cipher, then will assign the value of the cipher to the original character, forming the encryption, before moving onto the next 
+letter in the message. The reverse is done when encrypting messages */
+
+
 #include <stdio.h>
 
 main() 
@@ -20,7 +37,6 @@ main()
     scanf("%d", &selection); // compiler reads user's choice from above menu
     printf("You selected option %d\n", selection);
 
-  
     switch(selection) // SWITCH: indicates what compiler should execute for each of above menu option, e.g. if selection == 1; execute case 1
     {
         case 1: // Encryption of message w rotation cipher given message text and rotation amount
@@ -38,34 +54,22 @@ main()
             {
                 character = message[n];
         
-                if (65 <= character && character <= 90) //IF: if user inputs uppercase letter (numeric boundaries are ASCII standard values)
+                if ('A' <= character && character <= 'Z') //IF: if user inputs uppercase letter (numeric boundaries are ASCII standard values)
                 {
                     character += key;       // apply shift to character, operator shorthand (OS): character = character + key
                 
-                    if (character > 90)     // 90 is ASCII value for 'Z'
+                    if (character > 'Z')     // 90 is ASCII value for 'Z'
                     {     
                         character -= 26;    // wrap back to start of alphabet; 
                     }
                 
                     message[n] = character; // assign shifted value of character to letter
                 }
-             
-
-                else if (character >= 97 && character <= 122) // ELSE IF: if user inputs lowercase letter
-                {
-                    character += key;       // apply shift to character
-                    
-                    if (character > 122)    // 122 is ASCII value for 'z'
-                    {
-                        character -= 26;    // wrap back to start of alphabet, as if 'z' is number prior to 'a'       
-                    }   
-                    message[n] = character; // assign shifted value of character to letter
-            
-                }
-
-                printf("Your encrypted message is: %s\n", message);
+            }
+                
+            printf("Your encrypted message is: %s\n", message);
     
-                break;
+            break;
         
         case 2: // Decryption of message encrypted w rotation cipher given cipher text and rotation amount
    
@@ -90,19 +94,6 @@ main()
                     }
                 
                     message[n] = character; // assign reverse-shifted value of character to letter
-                }
-            
-                
-                else if (character >= 97 && character <= 122) // if user inputs lowercase character
-                {
-                    character -= key;      // apply shift backwards to character
-                
-                    if (character < 97)    // 97 is ASCII value for 'a'
-                    {
-                        character += 26;   //wrap back to start of alphabet, as if 'a' is number after to 'z'       
-                    }
-                
-                    message[n] = character; //assign reverse-shifted value of character to letter
                 }
             }
     
@@ -172,5 +163,4 @@ main()
     }
 
     return 0;
-
 }
